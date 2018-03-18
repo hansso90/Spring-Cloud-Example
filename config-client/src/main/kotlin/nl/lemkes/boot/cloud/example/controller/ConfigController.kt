6fd9d3d439f76dd25090ab64bebe8283c9409322
Hans Lemkes <hanslemkes@gmail.com>
2018-03-18 20:49:32 +0100

    - update ConfigController with RefreshScope

--- config-client/src/main/kotlin/nl/lemkes/boot/cloud/example/controller/ConfigController.kt
+++ config-client/src/main/kotlin/nl/lemkes/boot/cloud/example/controller/ConfigController.kt
@@ -1,9 +1,11 @@
 package nl.lemkes.boot.cloud.example.controller
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@RefreshScope
 @RestController
 class ConfigController
 {
